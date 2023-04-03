25e2a0195c5ed0c011d2dfbe59be9aaf89ef5887
Marcin <gasiorek.mail@gmail.com>
2023-04-03 10:45:54 +0200

    create server packet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            DatagramSocket socket = new DatagramSocket(5000);
+            while (true) {
+                byte[] buffer = new byte[50];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                System.out.println("Text recieved " + new String(buffer));
+            }
+
+        } catch (SocketException e) {
+            System.out.println("SocketException : " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
 }
